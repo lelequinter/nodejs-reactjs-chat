383dc334bdb352547eba0227948ff39c0cac1ff0
lelequinter <lele.quinter@gmail.com>
2023-04-15 16:29:53 -0500

    chat usando PrettyChatWindow

--- chat-frontend/src/ChatsPage.jsx
+++ chat-frontend/src/ChatsPage.jsx
@@ -1,20 +1,14 @@
-import {
-  MultiChatSocket,
-  MultiChatWindow,
-  useMultiChatLogic,
-} from "react-chat-engine-advanced";
+import { PrettyChatWindow } from "react-chat-engine-pretty";
 
 const ChatsPage = (props) => {
-  const chatProps = useMultiChatLogic(
-    "02c562a3-7f9f-4a5f-9739-120a52d5f826",
-    props.user.username,
-    props.user.secret
-  );
-
   return (
     <div style={{ height: "100vh" }}>
-      <MultiChatSocket {...chatProps} />
-      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+      <PrettyChatWindow
+        projectId="02c562a3-7f9f-4a5f-9739-120a52d5f826"
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
     </div>
   );
 };

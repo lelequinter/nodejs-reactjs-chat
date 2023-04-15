8ec312c9005cb05c74d84289a4724ad52d866044
lelequinter <lele.quinter@gmail.com>
2023-04-15 16:20:46 -0500

    chat funcional con chat engine-advanced

--- chat-frontend/src/ChatsPage.jsx
+++ chat-frontend/src/ChatsPage.jsx
@@ -1,5 +1,22 @@
-const ChatsPage = () => {
-  return <>Chats ...</>;
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "02c562a3-7f9f-4a5f-9739-120a52d5f826",
+    props.user.username,
+    props.user.secret
+  );
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
 };
 
 export default ChatsPage;
